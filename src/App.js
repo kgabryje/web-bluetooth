699349c1060adf3ed534547c83b2464d4ea9e5c6
Kamil Gabryjelski <kamil.gabryjelski@polidea.com>
2019-06-24 08:45:18 +0200

    added prettier + nicer formatting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, {useState} from 'react';
-import {SensorDataHandler} from "./SensorDataHandler";
+import React, { useState } from "react";
+import { SensorDataHandler } from "./SensorDataHandler";
 
 export const AccUUIDsContext = React.createContext();
 const ACC_UUIDS = {
-    serviceUUID: 'f000aa10-0451-4000-b000-000000000000',
-    dataCharUUID: 'f000aa11-0451-4000-b000-000000000000',
-    configCharUUID: 'f000aa12-0451-4000-b000-000000000000',
+  serviceUUID: "f000aa10-0451-4000-b000-000000000000",
+  dataCharUUID: "f000aa11-0451-4000-b000-000000000000",
+  configCharUUID: "f000aa12-0451-4000-b000-000000000000"
 };
 
 const App = () => {
-    const [accService, setAccService] = useState(undefined);
+  const [accService, setAccService] = useState(undefined);
 
-    const getService = serviceUUID => navigator.bluetooth.requestDevice({filters: [{services: [serviceUUID]}]})
-        .then(device => device.gatt.connect())
-        .then(server => server.getPrimaryService(serviceUUID))
-        .then(service => {
-            setAccService(service);
-            return service;
-        })
-        .catch(err => console.log(err.message));
+  const getService = serviceUUID =>
+    navigator.bluetooth
+      .requestDevice({ filters: [{ services: [serviceUUID] }] })
+      .then(device => device.gatt.connect())
+      .then(server => server.getPrimaryService(serviceUUID))
+      .then(service => {
+        setAccService(service);
+        return service;
+      })
+      .catch(err => console.log(err.message));
 
-    return (
-        <AccUUIDsContext.Provider value={ACC_UUIDS}>
-            <div>
-                <button onClick={() => getService(ACC_UUIDS.serviceUUID)}>Scan devices</button>
-            </div>
-            <SensorDataHandler service={accService}/>
-        </AccUUIDsContext.Provider>
-    );
+  return (
+    <AccUUIDsContext.Provider value={ACC_UUIDS}>
+      <div>
+        <button onClick={() => getService(ACC_UUIDS.serviceUUID)}>
+          Scan devices
+        </button>
+      </div>
+      <SensorDataHandler service={accService} />
+    </AccUUIDsContext.Provider>
+  );
 };
 
 export default App;
