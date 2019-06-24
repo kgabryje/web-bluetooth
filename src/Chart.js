1bb6ba08805121fd9adf4befbf36349cf7661f81
Kamil Gabryjelski <kamil.gabryjelski@polidea.com>
2019-06-24 08:55:37 +0200

    axis config for chart + nicer formatting

--- src/Chart.js
+++ src/Chart.js
@@ -1,40 +1,55 @@
-import React, {useState, useEffect, useRef} from "react"
-import CanvasJSReact from "./canvasjs.react"
+import React, { useState, useEffect, useRef } from "react";
+import CanvasJSReact from "./canvasjs.react";
 const CanvasChart = CanvasJSReact.CanvasJSChart;
 
-export const Chart = ({data}) => {
-    const chartRef = useRef(null);
-    const [dataPoints, setDataPoints] = useState([]);
-    const [dataIndex, setDataIndex] = useState(0);
+export const Chart = ({ data }) => {
+  const chartRef = useRef(null);
+  const [dataPoints, setDataPoints] = useState([]);
+  const [dataIndex, setDataIndex] = useState(0);
 
-    const updateChart = value => {
-        const point = {x: dataIndex, y: value};
+  const updateChart = value => {
+    const point = { x: dataIndex, y: value };
 
-        setDataPoints(prevDataPoints => {
-            const points = [...prevDataPoints, point];
-            if (points.length > 20) {
-                points.shift();
-            }
-            return points;
-        });
-        setDataIndex(prevDataIndex => prevDataIndex + 1);
-        chartRef.current.render()
-    };
+    setDataPoints(prevDataPoints => {
+      const points = [...prevDataPoints, point];
+      if (points.length > 20) {
+        points.shift();
+      }
+      return points;
+    });
+    setDataIndex(prevDataIndex => prevDataIndex + 1);
+    chartRef.current.render();
+  };
 
-    useEffect(() => {
-        if (chartRef && chartRef.current) {
-            updateChart(data);
-        }
-    }, [data]);
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      updateChart(data);
+    }
+  }, [data]);
 
-    const options = {
-        title: {
-            text: "Accelerometer data"
-        },
-        data: [{
-            type: "line",
-            dataPoints: dataPoints
-        }]
-    };
-    return (<CanvasChart ref={chartRef} options={options}/>);
+  const options = {
+    title: {
+      text: "Accelerometer data"
+    },
+    data: [
+      {
+        type: "line",
+        dataPoints: dataPoints
+      }
+    ],
+    axisX: {
+      title: "ticks",
+      minimum: 0,
+      interval: 1
+    },
+    axisY: {
+      title: "g",
+      minimum: 0
+    }
+  };
+  return (
+    <div style={{ margin: "32px" }}>
+      <CanvasChart ref={chartRef} options={options} />
+    </div>
+  );
 };
