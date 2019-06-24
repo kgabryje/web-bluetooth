cd8a7cd5f703e99511d44f9a18a6a89b1babb7d8
Kamil Gabryjelski <kamil.gabryjelski@polidea.com>
2019-06-24 14:49:47 +0200

    Updates

---
+++ precache-manifest.3a48c09f2793f4744cd1066ad9d57f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02b0a1fc09efefa994709dfe3301293",
+    "url": "/web-bluetooth/index.html"
+  },
+  {
+    "revision": "574a44cc2e09fbc75925",
+    "url": "/web-bluetooth/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "87919b4d01749af6fe80",
+    "url": "/web-bluetooth/static/js/2.0423afda.chunk.js"
+  },
+  {
+    "revision": "574a44cc2e09fbc75925",
+    "url": "/web-bluetooth/static/js/main.7d8b9230.chunk.js"
+  },
+  {
+    "revision": "5eb197a9f69b18b2c2fe",
+    "url": "/web-bluetooth/static/js/runtime~main.cf309ef5.js"
+  }
+]);
